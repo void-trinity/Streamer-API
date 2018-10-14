76239b67e085c4b97db29d50bc8eb88bc0dd61e9
void-trinity <chandrasiddharth2511@gmail.com>
2018-10-15 03:59:48 +0530

    Patched for pagination

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+
 mongoose.Promise = global.Promise;
 //mongoDB connection
 mongoose.connect('mongodb://localhost:27017/TweetStream', { useNewUrlParser: true });
 
-//mongo model
-var TweetData = mongoose.model('TweetData', {
+const schema = new mongoose.Schema ({
     created_at: {
         type: Date,
         required: true
@@ -32,7 +34,12 @@ var TweetData = mongoose.model('TweetData', {
     },
     favorite_count: {
         type: Number
-    },
+    }
 });
+
+schema.plugin(mongoosePaginate);
+
+//mongo model
+const TweetData = mongoose.model('TweetData', schema);
 
 module.exports = TweetData;
--- db/retrieveData.js
+++ db/retrieveData.js
@@ -2,7 +2,7 @@ const TweetData = require('./models/index');
 
 
 const retrieveData = async (search, filter, sort, page) => {
-    var result = await TweetData.find().sort({ date: 'asc' })
+    var result = await TweetData.paginate({}, { page: 2, limit: 10, sort: { created_at: -1 } })
     .then((data) => {
         return { data, success: true };
     }).catch((data) => {
