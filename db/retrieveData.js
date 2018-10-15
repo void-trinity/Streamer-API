13afdb18cc3e247dec8f60c6f7f426436dafc2a3
void-trinity <chandrasiddharth2511@gmail.com>
2018-10-15 13:31:30 +0530

    Patched Pagination and Added Tweer+User Search

--- db/retrieveData.js
+++ db/retrieveData.js
@@ -1,8 +1,29 @@
 const TweetData = require('./models/index');
 
 
-const retrieveData = async (search, filter, sort, page) => {
-    var result = await TweetData.paginate({}, { page: 2, limit: 10, sort: { created_at: -1 } })
+const retrieveData = async (search = {}, filter, sort = { created_at: -1 }, page = 1) => {
+    const { user, tweet } = search;
+    var query;
+    if(user && tweet)
+        query = {
+            text: new RegExp(`.*${tweet}.*`, 'i'),
+            $or: [
+                { 'user.name': new RegExp(`^${user}$`, 'i') },
+                { 'user.screen_name': new RegExp(`^${user}$`, 'i') }
+            ]
+        };
+    else if (user)
+        query = {
+            $or: [
+                { 'user.name': new RegExp(`^${user}$`, 'i') },
+                { 'user.screen_name': new RegExp(`^${user}$`, 'i') }
+            ]
+        };
+    else if (tweet)
+        query = { text: new RegExp(`.*${tweet}.*`, 'i') };
+    else
+        query = {};
+    var result = await TweetData.paginate(query, { page, limit: 10, sort })
     .then((data) => {
         return { data, success: true };
     }).catch((data) => {
